87188b91c4670fe41479975ad625dc9edcc1da9d
Madhan Adithya <madhan.adithya@ascendion.com>
2024-08-09 10:34:03 +0530

    material ui assignment

---
+++ material_ui_homework/App.js
@@ -0,0 +1,24 @@
+import MUIspacing from './MUIspacing';
+import AutoGrid from './AutoGrid';
+import BasicGrid from './BasicGrid';
+import ComplexFluidGrid from './ComplexFluidGrid';
+import FeaturedPost from './FeaturedPost';
+import MainFeaturedPost from './MainFeaturedPost';
+import NestedGridGroup from './NestedGridGroup';
+
+
+function App(){
+  return (
+    <>
+    <MUIspacing/>
+    <AutoGrid/>
+    <BasicGrid/>
+    <ComplexFluidGrid/>
+    <FeaturedPost/>
+    <MainFeaturedPost/>
+    <NestedGridGroup/>
+    </>
+  )
+}
+
+export default App;
